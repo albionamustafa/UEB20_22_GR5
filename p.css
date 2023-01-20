4b64ee3140b69d8a27161c39f23872139e69e6e7
Dion-Dauti <118399389+Dion-Dauti@users.noreply.github.com>
2023-01-20 16:57:58 +0100

    c

---
+++ p.css
@@ -0,0 +1,28 @@
+@import url('https://fonts.googleapis.com/css2?family=Kaisei+Tokumin:wght@400;500;700;800&family=League+Spartan:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Kaisei Tokumin', serif;
+    font-family: 'League Spartan', sans-serif;
+}
+
+
+.product img{
+    width: 100%;
+    height: auto;
+    box-sizing: border-box;
+    object-fit: cover;
+}
+
+.buy-btn{
+    margin-top: 30px;
+    width: 45%;
+    max-width: 100px;
+    background: linear-gradient(to right, rgb(116, 196, 224), rgb(74, 135, 175));
+    color: white;
+    font-size: 17px;
+    cursor: pointer;
+    border-radius: 3px;
+}
